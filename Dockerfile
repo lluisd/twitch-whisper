f6e2790b8c78a5de9a4089c94e730682745cd4b6
Lluis <lluisdanes@gmail.com>
2024-11-01 20:33:16 +0100

    initial whisper

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.9-slim
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get -y update
+RUN apt-get install build-essential ffmpeg git make wget  -y
+
+RUN git clone https://github.com/ggerganov/whisper.cpp.git whisper.cpp
+RUN cd whisper.cpp 
+RUN make -C ./whisper.cpp
+
+COPY . .
+
+EXPOSE 8000
+
+ENV NAME world
+
+CMD ["fastapi", "run"]
---
+++ main.py
@@ -0,0 +1,35 @@
+from typing import Union
+from fastapi import FastAPI
+import os
+import subprocess
+import signal
+import time
+
+#os.system('git clone https://github.com/ggerganov/whisper.cpp.git')
+#os.system('make -C ./whisper.cpp')
+#os.system('bash ./whisper.cpp/models/download-ggml-model.sh large-v3-turbo')
+#os.system('bash ./whisper.cpp/models/download-ggml-model.sh medium')
+#os.system('bash ./whisper.cpp/models/download-ggml-model.sh small')
+os.system('bash ./whisper.cpp/models/download-ggml-model.sh base')
+
+app = FastAPI()
+filename = ""
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/start/{channel}")
+def start(channel: str):
+    print(f'start recording audio from: {channel}')
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    global filename
+    filename = f'whisper-live{timestr}.wav'
+    app.proc = subprocess.Popen([f'streamlink https://www.twitch.tv/{channel} best --twitch-disable-ads -O 2>/dev/null | ffmpeg -loglevel quiet -i - -y -probesize 32 -y -ar 16000 -ac 1 -acodec pcm_s16le ./tmp/{filename}'], stdout=subprocess.PIPE, shell=False, preexec_fn=os.setsid)
+
+@app.get("/stop")
+def stop():
+    print("stop")
+    if filename:
+        os.killpg(os.getpgid(app.proc.pid), signal.SIGTERM) 
+        os.system(f'./whisper.cpp/main ./tmp/{filename} -t 4 -l es -m ./whisper.cpp/models/ggml-base.bin  --no-timestamps -otxt')
